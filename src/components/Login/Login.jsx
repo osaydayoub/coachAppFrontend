31d8a395c379d27e8fa0f2d0d20d5996f8c63102
osaydayoub <osaydayoub@gmail.com>
2024-01-20 00:45:58 +0200

    img + css

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import "../../pages/LoginPage/LoginPage.css";
 import { useAuth } from "../../context/AuthContext";
 import axios from "axios";
-
+import loginImg from "../../assets/msaCoach.jpeg"
 //TODO handel is afunc for moving to signup page oe login... so do it
 function Login({ handle }) {
   const [email, setEmail] = useState("");
@@ -36,8 +36,9 @@ function Login({ handle }) {
 
   return (
     <div className="login-container">
+        <img className='img-login' src={loginImg} alt="logo-img" />
       <h2>Welcome</h2>
-      <h3>M.S.A</h3>
+      {/* <h3>M.S.A</h3> */}
       {error && <div>{error}</div>}
       <form onSubmit={(e) => handleSubmit(e)}>
         <div>

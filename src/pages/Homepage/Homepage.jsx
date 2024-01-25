95e522dfe29135de5df2f9c6a315dd85575a7d8a
osaydayoub <osaydayoub@gmail.com>
2024-01-25 09:07:41 +0200

    responsive style added ...

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,27 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "../../context/AuthContext.jsx";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import "./Homepage.css";
+import { useData } from "../../context/DataContext.jsx";
+import { isSameDay } from "../../utils/helpers.js";
 function Homepage() {
-  const percentage1 = 10;
-  const percentage2 = 50;
-  const percentage3 = 80;
   const { currentUser, isLoggedIn } = useAuth();
+  const { currentClient } = useData();
+  const [dailyTracking, setDailyTracking] = useState(null);
+  useState(() => {
+    if (currentClient != null) {
+      let track = currentClient.dailyTracking.find((track) => {
+        return isSameDay(new Date(track.date), new Date());
+      });
+      if (track === undefined) {
+        console.log("stil no data");
+        const t = {
+          date: new Date(),
+          calories: 0,
+          waterAmount: 0,
+          sleepHours: 0,
+        };
+        track = t;
+      }
+      setDailyTracking(track);
+    }
+  }, []);
+
   return (
     <div className="Homepage page">
-      <h1>{`Hello ${currentUser.name} !`}</h1>
+      <h2>{`Hello ${currentUser.name} !`}</h2>
+      {dailyTracking && (
+        <p>{`Your caloric intake is currently at ${currentClient.caloricIntake}
+      , and you have consumed ${dailyTracking.calories} calories so far.`}</p>
+      )}
       {!currentUser.isAdmin && (
-        <div className="progress-container">
-          <div className="progress">
-            <CircularProgressbar value={percentage1} text={`${percentage1}%`} />
-          </div>
-          <div className="progress">
-            <CircularProgressbar value={percentage2} text={`${percentage2}%`} />
-          </div>
-          <div className="progress">
-            <CircularProgressbar value={percentage3} text={`${percentage3}%`} />
-          </div>
+        <div className="homepage-message-container">
+          <p className="daily-message-container">
+            Don't forget to log your calorie intake in your Daily Tracking.
+            Whether you're maintaining, gaining, or losing, keeping an eye on
+            your calories helps you stay on track with your health and fitness
+            goals.
+          </p>
+
+          {dailyTracking && (
+            <div className="progress-container">
+              <div className="progress">
+                <CircularProgressbar
+                  value={
+                    Math.floor((dailyTracking.calories / currentClient.caloricIntake)* 100)
+                  }
+                  text={`${
+                    Math.floor((dailyTracking.calories / currentClient.caloricIntake)* 100)
+                  }%`}
+                />
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
